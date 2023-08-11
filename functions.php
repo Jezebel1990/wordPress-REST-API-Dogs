171cc134751a895115092f710f99638c95ad6745
Jezebel1990 <jezebelguedes@gmail.com>
2023-08-11 01:37:03 -0300

    create user post

--- functions.php
+++ functions.php
@@ -2,6 +2,8 @@
 
 remove_action('rest_api_init', 'create_initial_rest_routes', 99);
 
+$dirbase = get_template_directory(); //puxa diretório do templete
+require_once $dirbase . '/endpoints/user_post.php'; // concatenar com . 
 
 function change_api() {
     return 'json';
